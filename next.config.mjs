206619e72d0c033ddf77a8045b68a1b014c1bf74
sallyq1 <96536341+sallyq1@users.noreply.github.com>
2024-10-18 22:23:43 -0500

    svgr-dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['edamam-product-images.s3.amazonaws.com'], // Add your domain here
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+        return config;
+    },
+};
 
 export default nextConfig;
