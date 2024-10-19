a5315fb9448d0443560ad2d3b27416fafeb746f3
TusharW4ni <reachtusharwani@gmail.com>
2024-10-18 22:11:54 -0500

    Refactor CSS and update course page layout

--- src/app/pages/course/page.tsx
+++ src/app/pages/course/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>Course page</div>
-  )
-}
+    <div className="flex justify-center">
+      <div className="w-1/2">
+        <div className="flex justify-center">
+          <h1 className="text-4xl">Spanish</h1>
+        </div>
+        <div className="flex justify-center">
+          <p className="text-lg">This is the course page</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
